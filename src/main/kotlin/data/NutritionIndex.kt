ce20511d6f6cb260fcffcfc39bf0f729c97c1e31
AdelTamer <adeltammer@gmail.com>
2025-04-15 15:13:07 +0200

    create NutritionIndex object class

---
+++ src/main/kotlin/data/NutritionIndex.kt
@@ -0,0 +1,11 @@
+package org.beijing.data
+
+object NutritionIndex {
+    const val CALORIES: Int = 0
+    const val TOTALFAT: Int = 1
+    const val SUGER: Int = 2
+    const val SODUM: Int = 3
+    const val PROTEIN: Int = 4
+    const val SATURATEEDFAT: Int = 5
+    const val CARBOHYDRATES: Int = 6
+}
