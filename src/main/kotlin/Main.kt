91a09823595d155cd5e2e0f43c1a828dd7fc3024
mohammed-akkad <momoo362514@gmail.com>
2025-04-15 12:48:06 +0300

    add initial cli menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
 package org.beijing
 
+import di.uiModule
 import org.beijing.di.appModule
+import org.beijing.presentation.FoodConsoleUi
 import org.koin.core.context.startKoin
+import org.koin.mp.KoinPlatform.getKoin
 
 fun main() {
 
     startKoin { modules(appModule) }
+
+    val ui: FoodConsoleUi = getKoin().get()
+    ui.start()
 
 }
--- src/main/kotlin/presentation/FoodConsoleUi.kt
+++ src/main/kotlin/presentation/FoodConsoleUi.kt
@@ -1,4 +1,43 @@
 package org.beijing.presentation
 
-class FoodConsoleUi {
+class FoodConsoleUi(
+    //here inject use cases objects
+) {
+
+    fun start() {
+        showWelcome()
+        presentFeatures()
+    }
+
+    private fun showWelcome() {
+        println("Welcome to Food Change Mood App")
+    }
+
+
+    private fun presentFeatures(startIndex: Int = 0) {
+
+        // Show the current page
+        println("\n\n ===Please enter one of the numbers listed below===\n")
+
+
+        print("here: ")
+        val input = getUserInput()
+
+        when(input) {
+            0 -> return
+            //write here your feature
+            else -> {
+                println("Invalid input.")
+            }
+        }
+
+        presentFeatures(startIndex)
+    }
+
+
+    private fun getUserInput(): Int? {
+        return readlnOrNull()?.toIntOrNull()
+    }
+
+
 }
