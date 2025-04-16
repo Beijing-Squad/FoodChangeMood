e490f4764e7e211b243b06367d103fdc5e3ba5ed
mohammed-akkad <momoo362514@gmail.com>
2025-04-16 12:19:09 +0300

    add get meals by calories and protein use cases in logic module

--- src/main/kotlin/di/LogicModule.kt
+++ src/main/kotlin/di/LogicModule.kt
@@ -1,8 +1,9 @@
 package di
 
+import logic.usecases.GetMealsByCaloriesAndProteinUseCases
 import org.koin.dsl.module
 
 val logicModule = module {
     // put features classes here
-
+    single { GetMealsByCaloriesAndProteinUseCases(get()) }
 }
