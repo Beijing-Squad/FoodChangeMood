1868096b2f8b193cd7442c81d21c82043825d86c
mohammed-akkad <momoo362514@gmail.com>
2025-04-16 14:43:36 +0300

    refactor get meals calories and protein

--- src/main/kotlin/logic/usecases/GetMealsByCaloriesAndProteinUseCases.kt
+++ src/main/kotlin/logic/usecases/GetMealsByCaloriesAndProteinUseCases.kt
@@ -8,14 +8,12 @@ class GetMealsByCaloriesAndProteinUseCases(
     private val mealRepository: MealRepository
 ) {
     fun getMealsByCaloriesAndProtein(targetCalories: Double, targetProtein: Double): List<Meal> {
-        if (targetCalories <= 0) throw Exception("Calories input must be positive")
-        if (targetProtein <= 0) throw Exception("Protein input must be positive")
+        if (targetCalories <= 0 || targetProtein <= 0) throw Exception("\nPlease ensure that both Calories and Protein inputs are positive values.")
 
         val matchPercentage = 0.5
         val ratio = 0.15
         return mealRepository.getAllMeals().filter { currentMeal ->
-            abs(currentMeal.nutrition.calories - targetCalories) * ratio <= matchPercentage &&
-                    abs(currentMeal.nutrition.protein - targetProtein) * ratio <= matchPercentage
+            abs(currentMeal.nutrition.calories - targetCalories) * ratio <= matchPercentage && abs(currentMeal.nutrition.protein - targetProtein) * ratio <= matchPercentage
         }
 
     }
