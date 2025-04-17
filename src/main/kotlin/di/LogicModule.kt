e9af8c0dae498d924cab0df05c26d9b093f9f054
karimdeabes <karam.zmzmy@gmail.com>
2025-04-17 13:28:10 +0200

    add get healthy quick meals use case to logic module

--- src/main/kotlin/di/LogicModule.kt
+++ src/main/kotlin/di/LogicModule.kt
@@ -1,8 +1,10 @@
 package di
 
+import org.beijing.logic.usecases.GetHealthyQuickMealsUseCase
 import org.koin.dsl.module
 
 val logicModule = module {
     // put features classes here
+    single { GetHealthyQuickMealsUseCase(get()) }
 
 }
