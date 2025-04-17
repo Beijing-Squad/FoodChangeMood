0d552bafc153b3f9b23cfa72e88d681211fff559
Radwa85 <radwa.mohamed5033@gmail.com>
2025-04-17 18:52:57 +0200

    added di of suggestion use cases

--- src/main/kotlin/di/LogicModule.kt
+++ src/main/kotlin/di/LogicModule.kt
@@ -1,9 +1,12 @@
 package di
 
 import org.beijing.logic.usecases.SearchMealsUseCases
+import org.beijing.logic.usecases.SuggestionMealsUseCases
 import org.koin.dsl.module
 
 val logicModule = module {
     // put features classes here
     single { SearchMealsUseCases(get()) }
+    single { SuggestionMealsUseCases(get()) }
+
 }
