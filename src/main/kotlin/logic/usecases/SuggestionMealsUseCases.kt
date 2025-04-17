86ccc3a5a9f66455c029fd1f4cf7bc32f749e95f
Radwa85 <radwa.mohamed5033@gmail.com>
2025-04-17 18:52:03 +0200

    implemented suggest keto diet meal function

--- src/main/kotlin/logic/usecases/SuggestionMealsUseCases.kt
+++ src/main/kotlin/logic/usecases/SuggestionMealsUseCases.kt
@@ -1,4 +1,22 @@
 package org.beijing.logic.usecases
 
-class SuggestionMealsUseCases {
+import org.beijing.logic.MealRepository
+import org.beijing.model.Meal
+import org.beijing.model.Nutrition
+
+class SuggestionMealsUseCases(
+    private val mealRepository: MealRepository
+) {
+    private val meals = mealRepository.getAllMeals()
+    fun suggestKetoMeal(usedMealIds: MutableSet<Int>): Meal? {
+        return meals
+            .filter { meal ->
+                meal.nutrition.carbohydrates < 20 &&
+                        meal.nutrition.totalFat > meal.nutrition.protein
+            }
+            .filterNot { it.id in usedMealIds }
+            .shuffled()
+            .firstOrNull()
+            ?.also { usedMealIds.add(it.id) }
+    }
 }
