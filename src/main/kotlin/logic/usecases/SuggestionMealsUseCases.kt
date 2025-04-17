512d0c353eb2608eed055e5d05176ad531199914
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-17 16:46:58 +0300

    add meals have more than seven hundred feature to use cases

--- src/main/kotlin/logic/usecases/SuggestionMealsUseCases.kt
+++ src/main/kotlin/logic/usecases/SuggestionMealsUseCases.kt
@@ -1,4 +1,18 @@
 package org.beijing.logic.usecases
 
-class SuggestionMealsUseCases {
+import org.beijing.logic.MealRepository
+import org.beijing.model.Meal
+
+class SuggestionMealsUseCases(
+    private val mealRepository: MealRepository
+) {
+
+    fun getMealsHaveMoreThanSevenHundredCalories(): List<Meal> {
+        val caloriesContentNeeded = 700
+        val filteredMeals = mealRepository.getAllMeals().filter { meal ->
+            meal.nutrition.calories >= caloriesContentNeeded
+        }
+        return filteredMeals
+    }
+
 }
