71f2190405b772093af6c3642cc81139a92adda5
AdelTamer <adeltammer@gmail.com>
2025-04-17 15:16:22 +0200

    inject search meals use cases

--- src/main/kotlin/di/LogicModule.kt
+++ src/main/kotlin/di/LogicModule.kt
@@ -1,8 +1,9 @@
 package di
 
+import org.beijing.logic.usecases.SearchMealsUseCases
 import org.koin.dsl.module
 
 val logicModule = module {
     // put features classes here
-
+    single { SearchMealsUseCases(get()) }
 }
