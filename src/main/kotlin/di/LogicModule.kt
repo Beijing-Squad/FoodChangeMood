eeb6d755c2f7ff5b8de28f3764f952ff7e64b3d5
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-17 14:04:27 +0200

    add kmp algorithm class in logicModule

--- src/main/kotlin/di/LogicModule.kt
+++ src/main/kotlin/di/LogicModule.kt
@@ -1,9 +1,12 @@
 package di
 
 import org.beijing.logic.usecases.SearchMealsUseCases
+import org.beijing.logic.usecases.utils.KmpSubstringSearch
 import org.koin.dsl.module
 
 val logicModule = module {
     // put features classes here
+
+    single { KmpSubstringSearch() }
     single { SearchMealsUseCases(get(),get())}
 }
