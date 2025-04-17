7f2b6ce8df6b2f168980c34ddb46fe2d119b1f70
AdelTamer <adeltammer@gmail.com>
2025-04-17 14:12:54 +0200

    format the code and update exception message

--- src/main/kotlin/logic/GetMealsByDateUseCase.kt
+++ src/main/kotlin/logic/GetMealsByDateUseCase.kt
@@ -6,13 +6,13 @@ class GetMealsByDateUseCase(
     private val mealRepository: MealRepository,
 ) {
 
-     fun getMealsByDate(date: LocalDate): List<Pair<Int, String>> {
+    fun getMealsByDate(date: LocalDate): List<Pair<Int, String>> {
         val mealsOnDate = mealRepository.getAllMeals()
             .filter { it.submitted == date }
             .map { Pair(it.id, it.name) }
 
         if (mealsOnDate.isEmpty()) {
-            throw Exception("No meals found for the date $date.")
+            throw Exception("❌ No Meals Found For The Date [$date].")
         } else {
             return mealsOnDate
         }
