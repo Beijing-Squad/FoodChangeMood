cce5f59d477708e000f3265c4d77486aa63a1f24
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-18 14:02:34 +0200

    add my feature implementation for large group food suggestions

--- src/main/kotlin/logic/usecases/SuggestionMealsUseCases.kt
+++ src/main/kotlin/logic/usecases/SuggestionMealsUseCases.kt
@@ -1,8 +1,18 @@
 package org.beijing.logic.usecases
 
 import org.beijing.logic.MealRepository
+import org.beijing.model.Meal
 
 class SuggestionMealsUseCases(
     private val mealRepository: MealRepository
 ) {
+
+    //region Italian Large Group Meals
+    fun getItalianLargeGroupsMeals(): List<Meal> {
+        return mealRepository.getAllMeals()
+            .filter {
+                "for-large-groups" in it.tags.map(String::lowercase) &&
+                        "italian" in it.tags.map(String::lowercase)
+            }
+    }//endregion
 }
