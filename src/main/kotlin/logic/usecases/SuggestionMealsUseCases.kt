701dae00dadc01da4db89e73d958cdac2e85633c
MohamedOmar989 <m.3.abdelfadeel@gmail.com>
2025-04-18 13:51:04 +0200

    add sweet with no eggs use case logic

--- src/main/kotlin/logic/usecases/SuggestionMealsUseCases.kt
+++ src/main/kotlin/logic/usecases/SuggestionMealsUseCases.kt
@@ -1,8 +1,30 @@
 package org.beijing.logic.usecases
 
 import org.beijing.logic.MealRepository
+import org.beijing.model.Meal
 
 class SuggestionMealsUseCases(
     private val mealRepository: MealRepository
 ) {
+    //region sweets with no eggs
+    fun getSweetWithNoEggs(): Meal? {
+        val meals = mealRepository.getAllMeals()
+        val seen = mutableSetOf<Int>()
+
+        val sweetsWithoutEggs = meals
+            .filter { meal ->
+                meal.tags.any { it.contains("sweet", ignoreCase = true) }
+            }
+            .filterNot { meal ->
+                meal.ingredients.any { it.contains("egg", ignoreCase = true) }
+            }
+            // Exclude already shown
+            .filterNot { seen.contains(it.id) }
+
+        val nextMeal = sweetsWithoutEggs.firstOrNull()
+        nextMeal?.let { seen.add(it.id) }
+
+        return nextMeal
+    }
+    //endregion
 }
