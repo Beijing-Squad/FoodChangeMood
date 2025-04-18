ae6ab3f017cc41416351f0ee4b1134264f80c612
mohammed-akkad <momoo362514@gmail.com>
2025-04-18 14:31:11 +0300

    update gym helper user interface

--- src/main/kotlin/presentation/service/SearchMealService.kt
+++ src/main/kotlin/presentation/service/SearchMealService.kt
@@ -1,20 +1,22 @@
 package org.beijing.presentation.service
 
 import org.beijing.logic.usecases.SearchMealsUseCases
+import org.beijing.model.Meal
+import org.koin.mp.KoinPlatform.getKoin
 
-fun searchMealService(searchMealsUseCases: SearchMealsUseCases) {
+fun searchMealService() {
 
     showOptionsForSearchMealService()
-    print("\nhere: \n")
+    print("\nhere: ")
     when (val input = getUserInput()) {
-        1 -> launchGymHelper(searchMealsUseCases)
+        1 -> launchGymHelper()
 
 
         0 -> return
 
         else -> println("Invalid input: $input")
     }
-    searchMealService(searchMealsUseCases)
+    searchMealService()
 }
 
 fun showOptionsForSearchMealService() {
@@ -28,18 +30,56 @@ private fun getUserInput(): Int? {
 }
 
 // region gym helper
-private fun launchGymHelper(searchMealsUseCases: SearchMealsUseCases) {
+private fun launchGymHelper() {
+    val searchMeals: SearchMealsUseCases = getKoin().get()
     print("enter target of Calories: ")
     val targetCalories = readlnOrNull()?.toDoubleOrNull()
-    print("enter target of Protein:")
+    print("enter target of Protein: ")
     val targetProtein = readlnOrNull()?.toDoubleOrNull()
     if (targetProtein != null && targetCalories != null) {
-        println(
-            searchMealsUseCases.getGymHelperMeals(
-                targetCalories,
-                targetProtein
-            )
+        val meals = searchMeals.getGymHelperMeals(
+            targetCalories, targetProtein
         )
+        showGymHelperResult(meals)
+
+    }
+}
+
+private fun showGymHelperResult(meals: List<Meal>) {
+    searchAgainAboutGymHelper(meals)
+    meals.forEachIndexed { indexOfMeal, currentMeal ->
+        println("\n${indexOfMeal + 1}- ${currentMeal.name}")
+        println("\t\t\t\t minutes: ${currentMeal.minutes}")
+        println("\t\t\t\t nutrition: ")
+        currentMeal.nutrition.also { currentNutrition ->
+            println("\t\t\t\t\t\t Calories: ${currentNutrition.calories}")
+            println("\t\t\t\t\t\t Protein: ${currentNutrition.protein}")
+        }
+        println("\n\t\t\t\t ingredients: ")
+        currentMeal.ingredients.forEachIndexed { indexOfIngredient, currentIngredient ->
+            println("\t\t\t\t\t\t ${indexOfIngredient + 1}- $currentIngredient")
+        }
+        println("\n\t\t\t\t steps: ")
+        currentMeal.steps.forEachIndexed { indexOfSteps, currentStep ->
+            println("\t\t\t\t\t\t ${indexOfSteps + 1}- $currentStep")
+        }
+
+    }
+}
+
+private fun searchAgainAboutGymHelper(meals: List<Meal>) {
+    if (meals.isEmpty()) {
+        println("\nNo meals found\n")
+        println("Do you want search again?")
+        println("\t1- Yes")
+        println("\t0- No")
+
+        print("\nhere: ")
+        when (getUserInput()) {
+            1 -> launchGymHelper()
+            0 -> return
+            else -> println("Invalid input")
+        }
     }
 }
 //endregion
