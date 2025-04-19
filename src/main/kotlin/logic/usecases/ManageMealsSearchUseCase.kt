81406a75cd4f118843a62d9c3ca7e457646eac1d
Mohamed Elsadany <mohamedali802060@gamil.com>
2025-04-19 14:03:18 +0200

    update gym helper

--- src/main/kotlin/logic/usecases/ManageMealsSearchUseCase.kt
+++ src/main/kotlin/logic/usecases/ManageMealsSearchUseCase.kt
@@ -29,16 +29,9 @@ class ManageMealsSearchUseCase(
 
     //region gym helper
     fun getGymHelperMeals(targetCalories: Double, targetProtein: Double): List<Meal> {
-        checkIfTargetCaloriesAndTargetProteinAreInvalid(targetCalories, targetProtein)
-
         return mealRepository.getAllMeals().filter { currentMeal ->
             isMealWithinNutritionTargets(currentMeal, targetCalories, targetProtein)
         }
-
-    }
-
-    private fun checkIfTargetCaloriesAndTargetProteinAreInvalid(targetCalories: Double, targetProtein: Double) {
-        if (targetCalories <= 0 || targetProtein <= 0) throw Exception(ERROR_MESSAGE)
     }
 
     private fun isMealWithinNutritionTargets(meal: Meal, targetCalories: Double, targetProtein: Double): Boolean {
@@ -50,7 +43,6 @@ class ManageMealsSearchUseCase(
         return abs(currentNutrition - targetNutrition) * RATIO
     }
     //endregion
-
 
     //region search by name
     fun getSearchMealsByName(searchQuery: String): List<Meal> {
