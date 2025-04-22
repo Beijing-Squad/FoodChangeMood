23065bd2ff3c096ecde476f85908831e20edd8f6
mohammed-akkad <momoo362514@gmail.com>
2025-04-22 17:42:27 +0300

    add search meal service and view meal service in ui module

--- src/main/kotlin/di/UIModule.kt
+++ src/main/kotlin/di/UIModule.kt
@@ -1,8 +1,18 @@
 package di
 
 import org.beijing.presentation.FoodConsoleUi
+import org.beijing.presentation.FoodUi
+import org.beijing.presentation.service.SearchMealService
+import org.beijing.presentation.service.ViewMealsService
 import org.koin.dsl.module
 
 val uiModule = module {
-    single { FoodConsoleUi() }
+    single { SearchMealService() }
+    single { ViewMealsService() }
+    single<FoodUi> {
+        FoodConsoleUi(
+            searchMealService = get(),
+            viewMealsService = get(),
+        )
+    }
 }
