bb008fff6bb5aabc64487437bc6b4702f6e1ceba
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-22 17:03:24 +0300

    refactor seafood feature use case

--- src/main/kotlin/logic/usecases/ManageMealsViewUseCase.kt
+++ src/main/kotlin/logic/usecases/ManageMealsViewUseCase.kt
@@ -43,10 +43,9 @@ class ManageMealsViewUseCase(
     // region get a list of seafood sorted by protein content
 
     fun getSortedSeaFoodByProtein(): List<Meal> {
-        val sortedSeaFood = mealRepository.getAllMeals()
+        return mealRepository.getAllMeals()
             .filter { it.tags.contains("seafood") }
             .sortedByDescending { it.nutrition.proteinGrams }
-        return sortedSeaFood
     }
     //endregion
 }
