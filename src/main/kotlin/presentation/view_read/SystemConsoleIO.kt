2b17efb7bbc17c618d090ce9082f57f2376b3519
mohammed-akkad <momoo362514@gmail.com>
2025-04-23 13:13:45 +0300

    refactor system console io

--- src/main/kotlin/presentation/view_read/SystemConsoleIO.kt
+++ src/main/kotlin/presentation/view_read/SystemConsoleIO.kt
@@ -1,7 +1,7 @@
 package presentation.view_read
 
 class SystemConsoleIO : ConsoleIO {
-    override fun view(message: String) = kotlin.io.print(message)
-    override fun viewWithLine(message: String?) = kotlin.io.println(message)
+    override fun view(message: String) = print(message)
+    override fun viewWithLine(message: String?) = println(message)
     override fun readInput(): String? = readlnOrNull()
 }
