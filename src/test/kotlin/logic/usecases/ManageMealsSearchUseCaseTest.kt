f217b2cbdbeb950db8dd95fcf7f3d8b1ef34f1e3
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-23 13:10:08 +0200

    create mock for meals search use case test

--- src/test/kotlin/logic/usecases/ManageMealsSearchUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/ManageMealsSearchUseCaseTest.kt
@@ -1,8 +1,21 @@
 package logic.usecases
 
+import io.mockk.mockk
+import org.beijing.logic.MealRepository
+import org.beijing.logic.usecases.ManageMealsSearchUseCase
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class ManageMealsSearchUseCaseTest {
+
+ private lateinit var mealRepository: MealRepository
+ private lateinit var useCase: ManageMealsSearchUseCase
+
+ @BeforeEach
+ fun setup(){
+  mealRepository = mockk()
+  useCase = ManageMealsSearchUseCase(mealRepository)
+ }
 
 @Test
  fun getMealsByDate() {}
