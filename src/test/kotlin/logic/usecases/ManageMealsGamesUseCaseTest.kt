b372b9ba91bf4371b439f71ba564fddd21fee4ec
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-23 13:06:35 +0200

    create mock for meals games use case test

--- src/test/kotlin/logic/usecases/ManageMealsGamesUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/ManageMealsGamesUseCaseTest.kt
@@ -1,8 +1,21 @@
 package logic.usecases
 
+import io.mockk.mockk
+import org.beijing.logic.MealRepository
+import org.beijing.logic.usecases.ManageMealsGamesUseCase
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class ManageMealsGamesUseCaseTest {
+
+ private lateinit var mealRepository: MealRepository
+ private lateinit var useCase: ManageMealsGamesUseCase
+
+ @BeforeEach
+ fun setup(){
+  mealRepository = mockk()
+  useCase = ManageMealsGamesUseCase(mealRepository)
+ }
 
 @Test
  fun startNewRound() {}
