007e824f96d353862cb8a9408a6735998fb2445f
mohammed-akkad <momoo362514@gmail.com>
2025-04-23 11:56:47 +0300

    refactor view meal service

--- src/main/kotlin/presentation/service/ViewMealsService.kt
+++ src/main/kotlin/presentation/service/ViewMealsService.kt
@@ -1,10 +1,12 @@
 package org.beijing.presentation.service
 
 import org.beijing.logic.usecases.ManageMealsViewUseCase
-import org.koin.mp.KoinPlatform.getKoin
+import presentation.view_read.ConsoleIO
 
-class ViewMealsService : MealService() {
-    private val viewMeals: ManageMealsViewUseCase = getKoin().get()
+class ViewMealsService(
+    private val viewMeals: ManageMealsViewUseCase,
+    private val consoleIO: ConsoleIO
+) : MealService(consoleIO) {
 
     override fun showService() {
         super.showService()
@@ -12,19 +14,19 @@ class ViewMealsService : MealService() {
     }
 
     override fun showOptionService() {
-        println("\n\n ===Please enter one of the numbers listed below===\n")
-        println("1. Show Healthy Quick Prepared Meals")
-        println("2. Show SeaFood Sorted By Protein Content")
-        println("0. Exit")
+        consoleIO.println("\n\n ===Please enter one of the numbers listed below===\n")
+        consoleIO.println("1. Show Healthy Quick Prepared Meals")
+        consoleIO.println("2. Show SeaFood Sorted By Protein Content")
+        consoleIO.println("0. Exit")
     }
 
     override fun handleUserChoice() {
-        print("\nhere: ")
-        when (getUserInput()) {
+        consoleIO.print("\nhere: ")
+        when (consoleIO.readInput()) {
             "1" -> launchHealthyQuickPreparedMeals()
             "2" -> showSortedSeaFoodByProtein()
             "0" -> return
-            else -> println("❌ Invalid input! Please enter a number between 0 and 2")
+            else -> consoleIO.println("❌ Invalid input! Please enter a number between 0 and 2")
         }
     }
 
@@ -33,11 +35,11 @@ class ViewMealsService : MealService() {
         val healthyQuickMeals = viewMeals.getHealthyQuickPreparedMeals()
 
         if (healthyQuickMeals.isEmpty()) {
-            println("There is no healthy quick-prepared meals.")
+            consoleIO.println("There is no healthy quick-prepared meals.")
         } else {
-            println("🍕🍔🍗 List of healthy quick-prepared meals \uD83C\uDF55\uD83C\uDF54\uD83C\uDF57")
-            println("-".repeat(120))
-            println(
+            consoleIO.println("🍕🍔🍗 List of healthy quick-prepared meals \uD83C\uDF55\uD83C\uDF54\uD83C\uDF57")
+            consoleIO.println("-".repeat(120))
+            consoleIO.println(
                 "Rank".padEnd(5) + "| " +
                         "Meal Name".padEnd(70) + "| " +
                         "Time".padEnd(6) + "| " +
@@ -45,10 +47,10 @@ class ViewMealsService : MealService() {
                         "Saturated Fat".padEnd(15) + "| " +
                         "Carbs"
             )
-            println("-".repeat(120))
+            consoleIO.println("-".repeat(120))
 
             healthyQuickMeals.forEachIndexed { index, meal ->
-                println(
+                consoleIO.println(
                     "${(index + 1).toString().padEnd(5)}| " +
                             meal.name.padEnd(70) + "| " +
                             "${meal.minutes}m".padEnd(6) + "| " +
@@ -64,11 +66,11 @@ class ViewMealsService : MealService() {
 // region get a list of seafood sorted by protein content
 
     fun showSortedSeaFoodByProtein() {
-        println("List Of SeaFood Sorted By Protein:")
-        println(String.format("%-6s| %-70s | %-14s", "Rank", "Meal Name", "Protein Content"))
-        println("----------------------------------------------------------------------------------------------------")
+        consoleIO.println("List Of SeaFood Sorted By Protein:")
+        consoleIO.println(String.format("%-6s| %-70s | %-14s", "Rank", "Meal Name", "Protein Content"))
+        consoleIO.println("----------------------------------------------------------------------------------------------------")
         viewMeals.getSortedSeaFoodByProtein().forEachIndexed { index, meal ->
-            println(String.format("%-6d| %-70s | %-14d", index + 1, meal.name, meal.nutrition.proteinGrams.toInt()))
+            consoleIO.println(String.format("%-6d| %-70s | %-14d", index + 1, meal.name, meal.nutrition.proteinGrams.toInt()))
         }
     }
 //endregion
