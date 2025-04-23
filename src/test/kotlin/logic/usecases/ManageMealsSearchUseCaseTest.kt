feb879424f7de43caeffd9d81638134db3620b0f
AdelTamer <adeltammer@gmail.com>
2025-04-23 17:57:29 +0200

    write test cases for get meal by date

--- src/test/kotlin/logic/usecases/ManageMealsSearchUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/ManageMealsSearchUseCaseTest.kt
@@ -1,10 +1,15 @@
 package logic.usecases
 
+import fake.mealsWithDate
+import io.mockk.every
 import io.mockk.mockk
 import org.beijing.logic.MealRepository
 import org.beijing.logic.usecases.ManageMealsSearchUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class ManageMealsSearchUseCaseTest {
 
@@ -19,33 +24,82 @@ class ManageMealsSearchUseCaseTest {
 
     //region get meal by date
     @Test
-    fun getMealsByDate() {
+    fun `should return list of meals when getMealsByDate called with valid date`() {
+        // Given
+        every { mealRepository.getAllMeals() } returns mealsWithDate
+
+        // When
+        val result = useCase.getMealsByDate("2023-05-10")
+
+        // Then
+        assert(result.size == 2)
     }
-//endregion
+
+    @Test
+    fun `should throw exception when no meals found on the date`() {
+        // Given
+        every { mealRepository.getAllMeals() } returns mealsWithDate
+
+        // When && Then
+        assertThrows<Exception> {
+            useCase.getMealsByDate("2024-06-01")
+        }
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(
+        "2023 05 10",
+        "2023@05@10",
+        "20230615",
+        "2023-60-01",
+        "2023/01/02"
+    )
+    fun `should throw IllegalArgumentException when getMealsByDate called with invalid date`(date: String) {
+        // Given
+        every { mealRepository.getAllMeals() } returns mealsWithDate
+
+        // When && Then
+        assertThrows<IllegalArgumentException> {
+            useCase.getMealsByDate(date)
+        }
+    }
+
+    @Test
+    fun `should throw exception when the date is greater than current date`() {
+        // Given
+        every { mealRepository.getAllMeals() } returns mealsWithDate
+
+        // When && Then
+        assertThrows<Exception> {
+            useCase.getMealsByDate("2030-04-23")
+        }
+    }
+    //endregion
 
     //region get meal by date and id
     @Test
     fun getMealByDateAndId() {
     }
-//endregion
+    //endregion
 
     //region get gym helper meals by calories
     @Test
     fun getGymHelperMealsByCaloriesAndProtein() {
     }
-//endregion
+    //endregion
 
     // region get meal by name
     @Test
     fun getMealByName() {
     }
-//endregion
+    //endregion
 
     //region get meal by country
     @Test
     fun getMealByCountry() {
     }
-//endregion
+    //endregion
 
     //region get iraqi meals
     @Test
