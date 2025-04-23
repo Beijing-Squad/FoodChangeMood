cf16694a3951581f0abfcea2615e14690d1ddf02
mohammed-akkad <momoo362514@gmail.com>
2025-04-23 11:51:32 +0300

    refactor food console ui

--- src/main/kotlin/presentation/FoodConsoleUi.kt
+++ src/main/kotlin/presentation/FoodConsoleUi.kt
@@ -2,12 +2,14 @@ package org.beijing.presentation
 
 
 import org.beijing.presentation.service.*
+import presentation.view_read.ConsoleIO
 
 class FoodConsoleUi(
     private val searchMealService: SearchMealService,
     private val viewMealsService: ViewMealsService,
     private val gameMealsService: GameMealsService,
-    private val suggestionMealsService: SuggestionMealsService
+    private val suggestionMealsService: SuggestionMealsService,
+    private val consoleIO: ConsoleIO
 ) : FoodUi {
     override fun start() {
         showWelcome()
@@ -15,42 +17,37 @@ class FoodConsoleUi(
         try {
             presentFeatures()
         } catch (e: Exception) {
-            println(e.message)
+            consoleIO.println(e.message)
         }
     }
 
     private fun showWelcome() {
-        print("Welcome to Food Change Mood\uD83E\uDD6A ")
+        consoleIO.print("Welcome to Food Change Mood\uD83E\uDD6A ")
     }
 
     private fun presentFeatures() {
         showOptions()
-        when (getUserInput()) {
-            1 -> suggestionMealsService.showService()
-            2 -> searchMealService.showService()
-            3 -> gameMealsService.showService()
-            4 -> viewMealsService.showService()
-            0 -> return
+        when (consoleIO.readInput()) {
+            "1" -> suggestionMealsService.showService()
+            "2" -> searchMealService.showService()
+            "3" -> gameMealsService.showService()
+            "4" -> viewMealsService.showService()
+            "0" -> return
             else -> {
-                println("❌ Invalid input! Please enter a number between 0 and 4")
+                consoleIO.println("❌ Invalid input! Please enter a number between 0 and 4")
             }
         }
         presentFeatures()
     }
 
     private fun showOptions() {
-        println("\n\n===Please enter one of the numbers listed below===\n")
-        println("1. Suggestion Meal \uD83E\uDD14")
-        println("2. Search Meal \uD83D\uDD0E")
-        println("3. Game Meal \uD83C\uDFAE")
-        println("4. View Meal \uD83E\uDD63")
-        println("0. Exit")
-        print("\nhere: ")
-    }
-
-
-    private fun getUserInput(): Int? {
-        return readlnOrNull()?.toIntOrNull()
+        consoleIO.println("\n\n===Please enter one of the numbers listed below===\n")
+        consoleIO.println("1. Suggestion Meal \uD83E\uDD14")
+        consoleIO.println("2. Search Meal \uD83D\uDD0E")
+        consoleIO.println("3. Game Meal \uD83C\uDFAE")
+        consoleIO.println("4. View Meal \uD83E\uDD63")
+        consoleIO.println("0. Exit")
+        consoleIO.print("\nhere: ")
     }
 
 }
