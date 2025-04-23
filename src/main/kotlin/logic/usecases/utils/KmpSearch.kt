904985ad9353f7e7cf040a1f952d3a3d68bb0a0f
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-23 11:35:29 +0200

    refactor kmp algorithm

--- src/main/kotlin/logic/usecases/utils/KmpSearch.kt
+++ src/main/kotlin/logic/usecases/utils/KmpSearch.kt
@@ -10,23 +10,30 @@ object KmpSearch {
         var patternIndex = 0
 
         while (textIndex < searchedFullText.length) {
-            if (searchedPattern[patternIndex] == searchedFullText[textIndex]) {
-                textIndex++
-                patternIndex++
-            }
-
-            if (patternIndex == searchedPattern.length) {
-                return true
-            } else if (textIndex < searchedFullText.length && searchedPattern[patternIndex] != searchedFullText[textIndex]) {
-                if (patternIndex != 0) {
-                    // Use LPS array to avoid redundant comparisons
-                    patternIndex = longestPrefixSuffix[patternIndex - 1]
-                } else {
+            when {
+                searchedPattern[patternIndex] == searchedFullText[textIndex] -> {
                     textIndex++
+                    patternIndex++
+                }
+
+                patternIndex == searchedPattern.length -> {
+                    return true
+                }
+
+                textIndex < searchedFullText.length && searchedPattern[patternIndex] != searchedFullText[textIndex] -> {
+                    when {
+                        patternIndex != 0 -> {
+                            patternIndex = longestPrefixSuffix[patternIndex - 1]
+                        }
+                        else -> {
+                            textIndex++
+                        }
+                    }
                 }
             }
         }
-        return false
+
+        return patternIndex == searchedPattern.length
     }
 
     private fun computeLpsArray(wordToSearchBy: String): IntArray {
@@ -35,15 +42,18 @@ object KmpSearch {
         var currentIndex = 1
 
         while (currentIndex < wordToSearchBy.length) {
-            if (wordToSearchBy[currentIndex] == wordToSearchBy[prefixLength]) {
-                prefixLength++
-                lps[currentIndex] = prefixLength
-                currentIndex++
-            } else {
-                if (prefixLength != 0) {
-                    // Fall back to previous LPS value
+            when {
+                wordToSearchBy[currentIndex] == wordToSearchBy[prefixLength] -> {
+                    prefixLength++
+                    lps[currentIndex] = prefixLength
+                    currentIndex++
+                }
+
+                prefixLength != 0 -> {
                     prefixLength = lps[prefixLength - 1]
-                } else {
+                }
+
+                else -> {
                     lps[currentIndex] = 0
                     currentIndex++
                 }
