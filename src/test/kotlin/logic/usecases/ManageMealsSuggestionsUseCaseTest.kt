5eb943f92199411f13f3a3f8fb26ab55982f11cd
karimdeabes <karam.zmzmy@gmail.com>
2025-04-24 16:59:34 +0200

    create test cases for get ten random meals contains potato

--- src/test/kotlin/logic/usecases/ManageMealsSuggestionsUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/ManageMealsSuggestionsUseCaseTest.kt
@@ -1,10 +1,16 @@
 package logic.usecases
 
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
 import io.mockk.mockk
+import mealsWithoutPotato
+import nineMealsContainsPotato
+import mealsContainsPotato
 import org.beijing.logic.MealRepository
 import org.beijing.logic.usecases.ManageMealsSuggestionsUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class ManageMealsSuggestionsUseCaseTest {
 
@@ -23,10 +29,57 @@ class ManageMealsSuggestionsUseCaseTest {
     }
 //endregion
 
-    //region suggest ten random meals contains potato
+    //region suggest ten random meals contains potato test
     @Test
-    fun suggestTenRandomMealsContainsPotato() {
+    fun `should return meals containing potato when have it`() {
+        //given
+        every { mealRepository.getAllMeals() } returns mealsContainsPotato
+
+        //when
+        val result = useCase.suggestTenRandomMealsContainsPotato()
+
+        //then
+        result.forEach { meal ->
+            assertThat(meal.ingredients).contains("potato")
+        }
+
     }
+
+    @Test
+    fun `should throw exception when meals does not contains potato`() {
+        //given
+        every { mealRepository.getAllMeals() } returns mealsWithoutPotato
+
+        //when & then
+        assertThrows<IllegalArgumentException> {
+            useCase.suggestTenRandomMealsContainsPotato()
+        }
+    }
+
+    @Test
+    fun `should return ten random meals when passed meals contains potato`() {
+        //given
+        every { mealRepository.getAllMeals() } returns mealsContainsPotato
+
+        //when
+        val result = useCase.suggestTenRandomMealsContainsPotato()
+
+        //then
+        assertThat(result.size).isEqualTo(10)
+
+    }
+
+    @Test
+    fun `should throw exception when return empty list or smaller than ten meals contains potato`(){
+        //given
+        every { mealRepository.getAllMeals() } returns nineMealsContainsPotato
+
+        //when && then
+        assertThrows<IllegalArgumentException>{
+            useCase.suggestTenRandomMealsContainsPotato()
+        }
+    }
+
 //endregion
 
     //region suggest italian large group meals
