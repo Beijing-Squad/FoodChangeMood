e0e92c23c45dc4bbfbcd96422392a51980e63a03
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-25 01:51:41 +0300

    add before each setup function and some needed dependencies

--- src/test/kotlin/presentation/service/SuggestionMealsServiceTest.kt
+++ src/test/kotlin/presentation/service/SuggestionMealsServiceTest.kt
@@ -1,12 +1,46 @@
 package presentation.service
 
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.beijing.logic.usecases.ManageMealsSuggestionsUseCase
+import org.beijing.presentation.ViewMealDetails
+import org.beijing.presentation.service.SuggestionMealsService
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import presentation.view_read.ConsoleIO
 
 class SuggestionMealsServiceTest {
+    private lateinit var manageMealsSuggest: ManageMealsSuggestionsUseCase
+    private lateinit var suggestMealService: SuggestionMealsService
+    private lateinit var viewMealDetails: ViewMealDetails
+    private lateinit var consoleIO: ConsoleIO
+
+    @BeforeEach
+    fun setup() {
+        manageMealsSuggest = mockk(relaxed = true)
+        viewMealDetails = mockk()
+        consoleIO = mockk(relaxed = true)
+        suggestMealService = SuggestionMealsService(manageMealsSuggest, viewMealDetails, consoleIO)
+    }
 
     //region handle user choice
     @Test
-    fun handleUserChoice() {
+    fun `should show option of sub feature when suggest meal selected`() {
+        // When
+        suggestMealService.showOptionService()
+
+        // Then
+        verify {
+            consoleIO.viewWithLine("\n\n===Please enter one of the numbers listed below===\n")
+            consoleIO.viewWithLine("1. Suggest a Keto Meal \uD83E\uDD51 ")
+            consoleIO.viewWithLine("2. Suggest Sweets with No Eggs \uD83C\uDF70")
+            consoleIO.viewWithLine("3. Suggest Easy Food \uD83C\uDF2D")
+            consoleIO.viewWithLine("4. Suggest Italian Meals for Large Groups \uD83C\uDF55")
+            consoleIO.viewWithLine("5. Suggest Ten Meals Contains Potato In Ingredients \uD83E\uDD54")
+            consoleIO.viewWithLine("6. Suggest Meal With more than 700 calories \uD83C\uDF54")
+            consoleIO.viewWithLine("0. Exit")
+        }
     }
     //endregion
 
