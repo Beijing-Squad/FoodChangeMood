9bf87ccf52560f9d10d2a95ddeff58f27c7f2bc9
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-24 20:47:42 +0300

    add so thin problem usecase tests

--- src/test/kotlin/logic/usecases/ManageMealsSuggestionsUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/ManageMealsSuggestionsUseCaseTest.kt
@@ -1,6 +1,11 @@
 package logic.usecases
 
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
 import io.mockk.mockk
+import logic.usecases.fakedata.mealsListWithHighCaloriesMeals
+import logic.usecases.fakedata.mealsWithHighCalories
+import logic.usecases.fakedata.mealsWithNoHighCalories
 import org.beijing.logic.MealRepository
 import org.beijing.logic.usecases.ManageMealsSuggestionsUseCase
 import org.junit.jupiter.api.BeforeEach
@@ -49,9 +54,35 @@ class ManageMealsSuggestionsUseCaseTest {
 
     //region suggest meal have more than seven hundred calories
     @Test
-    fun suggestMealHaveMoreThanSevenHundredCalories() {
+    fun `should return meals with high calories When give list of meals`() {
+        //Given
+        every { mealRepository.getAllMeals() } returns mealsListWithHighCaloriesMeals
+        //When
+        val result = useCase.suggestMealHaveMoreThanSevenHundredCalories()
+        //Then
+        assertThat(result).containsExactlyElementsIn(mealsWithHighCalories)
     }
-//endregion
+
+    @Test
+    fun `should return empty list When give empty list`() {
+        //Given
+        every { mealRepository.getAllMeals() } returns listOf()
+        //When
+        val result = useCase.suggestMealHaveMoreThanSevenHundredCalories()
+        //Then
+        assertThat(result).isEmpty()
+    }
+
+    @Test
+    fun `should return empty list When give list of meals have low calories`() {
+        //Given
+        every { mealRepository.getAllMeals() } returns mealsWithNoHighCalories
+        //When
+        val result = useCase.suggestMealHaveMoreThanSevenHundredCalories()
+        //Then
+        assertThat(result).isEmpty()
+    }
+    //endregion
 
     //region check meal calories content
     @Test
