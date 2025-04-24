9134e29e5cf3f59ccbf1a1d32fcce68284fa34e6
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-25 01:47:09 +0300

    add ui test cases of ten random potato meals

--- src/test/kotlin/presentation/service/SuggestionMealsServiceTest.kt
+++ src/test/kotlin/presentation/service/SuggestionMealsServiceTest.kt
@@ -9,4 +9,39 @@ class SuggestionMealsServiceTest {
     fun handleUserChoice() {
     }
     //endregion
+
+    //region ten random potato meals ui test
+    @Test
+    fun `should call get ten random potato meals when selected`() {
+        // Given
+        val choiceSearchFeature = "5"
+        val exitInput = "0"
+
+        every { consoleIO.readInput() } returns choiceSearchFeature andThen exitInput
+
+        // When
+        suggestMealService.handleUserChoice()
+
+        // Then
+        verify {
+            manageMealsSuggest.suggestTenRandomMealsContainsPotato()
+        }
+    }
+
+    @Test
+    fun `should show the error message when throw an exception`() {
+        // Given
+        val errorMessage = "no meals contains potato found"
+
+        every { manageMealsSuggest.suggestTenRandomMealsContainsPotato() } throws IllegalArgumentException(errorMessage)
+
+        // When
+        suggestMealService.launchTenRandomPotatoMeals()
+
+        // Then
+        verify {
+            consoleIO.viewWithLine(errorMessage)
+        }
+    }
+    //endregion
 }
