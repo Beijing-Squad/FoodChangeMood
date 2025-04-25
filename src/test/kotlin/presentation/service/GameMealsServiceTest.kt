466d7ff1448564d0315fb6f1a8e05130f61140af
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-25 16:18:25 +0300

    add test case when user guess is correct from the first guess

--- src/test/kotlin/presentation/service/GameMealsServiceTest.kt
+++ src/test/kotlin/presentation/service/GameMealsServiceTest.kt
@@ -1,6 +1,11 @@
 package presentation.service
 
+import com.google.common.base.Verify.verify
+import fake.createMeal
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
+import model.GameRound
 import org.beijing.logic.usecases.ManageMealsGamesUseCase
 import org.beijing.presentation.service.GameMealsService
 import org.junit.jupiter.api.BeforeEach
@@ -23,5 +28,36 @@ class GameMealsServiceTest {
     @Test
     fun handleUserChoice() {
     }
+    //endregion
+
+    //region Preparation Time
+    @Test
+    fun `should complete game immediately when user guesses correctly`() {
+        // Given
+        val meal = createMeal(name = "Kebab", minutes = 25)
+        val round1 = GameRound(meal, attemptsLeft = 3, isCompleted = false, lastFeedBack = null)
+        val round2 = round1.copy(
+            attemptsLeft = 2,
+            isCompleted = true,
+            lastFeedBack = "Correct!! The preparation time is indeed 25 minutes."
+        )
+
+        every { gamesMeals.startNewRound() } returns round1
+        every { consoleIO.readInput() } returnsMany listOf("25")
+        every { gamesMeals.makeGuess(round1, 25) } returns round2
+
+        // When
+        service.launchGuessGame()
+
+        // Then
+        verify {
+            consoleIO.viewWithLine("\n🎯 Guess the Preparation Time for: **Kebab** (in minutes)")
+            consoleIO.viewWithLine("You have 3 attempts!")
+            consoleIO.view("Your guess: ")
+            consoleIO.viewWithLine("Correct!! The preparation time is indeed 25 minutes.")
+            consoleIO.viewWithLine("🎮 Game Over! Returning to main menu.\n")
+        }
+    }
+
     //endregion
 }
