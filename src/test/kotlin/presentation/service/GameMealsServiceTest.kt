43e32a225546a41b2689dbab0a64cce01b47dd1c
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-25 16:07:06 +0300

    add structure environment for the GamesMealsService test cases

--- src/test/kotlin/presentation/service/GameMealsServiceTest.kt
+++ src/test/kotlin/presentation/service/GameMealsServiceTest.kt
@@ -1,9 +1,24 @@
 package presentation.service
 
+import io.mockk.mockk
+import org.beijing.logic.usecases.ManageMealsGamesUseCase
+import org.beijing.presentation.service.GameMealsService
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import presentation.view_read.ConsoleIO
 
 class GameMealsServiceTest {
 
+    private lateinit var gamesMeals: ManageMealsGamesUseCase
+    private lateinit var consoleIO: ConsoleIO
+    private lateinit var service: GameMealsService
+
+    @BeforeEach
+    fun setup() {
+        gamesMeals = mockk(relaxed = true)
+        consoleIO = mockk(relaxed = true)
+        service = GameMealsService(gamesMeals, consoleIO)
+    }
     //region handle user choice
     @Test
     fun handleUserChoice() {
