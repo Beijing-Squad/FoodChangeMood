30d51b0f36f8d1c17da56883e44b2cb098304461
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-25 20:14:05 +0300

    refactor code

--- src/test/kotlin/logic/usecases/ManageMealsSearchUseCaseTest.kt
+++ src/test/kotlin/logic/usecases/ManageMealsSearchUseCaseTest.kt
@@ -10,8 +10,6 @@ import io.mockk.mockk
 import kotlinx.datetime.LocalDate
 import org.beijing.logic.MealRepository
 import org.beijing.logic.usecases.ManageMealsSearchUseCase
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
